cfa1bd235f6de212c3c026a5cd03bdb27547e741
KncskBenedek <koncsikbenedek@gmail.com>
2022-03-29 10:42:41 +0200

    probléma megoldva

--- kecske.js
+++ kecske.js
@@ -1,5 +1,5 @@
 var tombSrc = [];
-var kepTomb = [];
+
 function  ID(elem){
     return document.getElementById(elem);
 }
@@ -9,8 +9,6 @@ window.addEventListener("load", init);
 function init(){
     ID("title").innerHTML = "Koncsik Benedek, Dervalics Attila";
     eventBe();
-    kiemel();
-    keretHover();
     rarakKiemel();
 }
 
@@ -36,17 +34,9 @@ function eventBe(){
 
 
 
-function keretHover(){
 
-
-    kepTomb.push();
-    for (let index = 0; index < tombSrc.length; index++) {
-        console.log(kepTomb[index])  
-    }
-
-}
 function rarakKiemel(){
-    console.log("rak mkiemel");
+    
     for (let index = 0; index < 3; index++) {
         $("img")[index].addEventListener("mouseover", kiemel);
         $("img")[index].addEventListener("mouseout", kiemelLeszed);
@@ -56,8 +46,18 @@ function rarakKiemel(){
 function kiemel(){
     var i = 0;
         var melyik = event.target.src;
+        
         while (i < 3 && !(melyik.includes($("img")[i].src))) {
             i++;
         }
         $("img")[i].style.border = "2px solid red";
+
+}
+
+function kiemelLeszed(){
+    for (let index = 0; index < 3; index++) {
+        $("img")[index].style.border = "0px solid red";
+    }
+    
+
 }
