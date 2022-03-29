41e13e51a455a12ad0e4467dec07aad3fb6efc0d
KncskBenedek <koncsikbenedek@gmail.com>
2022-03-29 09:20:47 +0200

    Tömbe mentés src

--- kecske.js
+++ kecske.js
@@ -1,3 +1,5 @@
+var tombSrc = [];
+
 function  ID(elem){
     return document.getElementById(elem);
 }
@@ -6,5 +8,25 @@ window.addEventListener("load", init);
 
 function init(){
     ID("title").innerHTML = "Koncsik Benedek, Dervalics Attila";
+    eventBe();
 }
 
+function $(elem){
+    return document.querySelectorAll(elem);
+}
+
+function tombAd(){
+    var src = event.target.src;+
+    //console.log(src);
+    tombSrc.push(src);
+    for (let index = 0; index < tombSrc.length; index++) {
+        console.log(tombSrc[index]);
+        
+    }
+}
+
+function eventBe(){   
+    for (let index = 0; index < 3; index++) {
+        $("img")[index].addEventListener("click", tombAd);
+    }
+}
